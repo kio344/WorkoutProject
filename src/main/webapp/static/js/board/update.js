6e1acf22be74794fa6aeb4e41d4fdb1d9cbad361
Munsu0701 <489700@naver.com>
2022-09-12 17:55:56 +0900

    몇가지 수정(페이지네이션 오류 및 삭제버튼 동작 방식 변경)

--- src/main/webapp/static/js/board/update.js
+++ src/main/webapp/static/js/board/update.js
@@ -1,31 +1,45 @@
-window.addEventListener("DOMContentLoaded", function(){
+window.addEventListener("DOMContentLoaded", function() {
 	const btnEl = document.getElementsByClassName("btn");
-	
-	for(btn of btnEl) {
+
+	for (btn of btnEl) {
 		btn.addEventListener("click", function(e) {
 			const target = e.target;
 			const parent = target.parentElement;
 			const abs = parent.previousElementSibling.firstChild.nextElementSibling;
 			abs.removeAttribute("disabled");
-			
+
 			const id = target.dataset.id;
 			const gid = target.dataset.gid;
-			
+
 			const div = document.createElement("div");
 			const upd = document.createElement("button");
-			const del = document.createElement("a");
-			
+			const del = document.createElement("button");
+
 			upd.setAttribute("type", "submit");
-			del.setAttribute("href", "../comment/delete?id=" + id + "&gid=" + gid);
-			
+			del.setAttribute("type", "button");
+
+			del.addEventListener("click", function() {
+				if(!confirm("정말 댓글을 삭제하시겠습니까?")) {
+					return;
+				}
+				const xhr = new XMLHttpRequest();
+				xhr.open("GET", "../comment/delete?id=" + id + "&gid=" + gid);
+				xhr.addEventListener("readystatechange", function() {
+					if (xhr.status == 200 && xhr.readyState == XMLHttpRequest.DONE) {
+						location.reload();
+					}
+				});
+				xhr.send(null);
+			});
+
 			upd.innerText = "수정하기";
 			del.innerText = "삭제하기";
-			
+
 			div.appendChild(upd);
 			div.appendChild(del);
 			parent.appendChild(div);
 			parent.removeChild(target);
-			
+
 		});
 	}
 });
